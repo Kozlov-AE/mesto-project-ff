1ff3891bd94356494af23592443642eb4d8d7fdf
Kozlov-AE <allek-007@list.ru>
2024-07-05 13:47:36 +0300

    Наяал делать

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,25 @@
 
 // @todo: DOM узлы
 
+
 // @todo: Функция создания карточки
+function createCard(name, link) {
+   const cardtemplate = document.querySelector('#card-template').content;
+   const templClone = cardtemplate.querySelector('.card').cloneNode(true);
+   const delButton = cardtemplate.querySelector('.card__delete-button');
+   delButton.addEventListener('click', deleteCard);
+   
+   const tTitle = templClone.querySelector('.card__title');
+   tTitle.insertAdjacentText = name;
+   
+   const tImg = templClone.querySelector('.card__image');
+   tImg.src = link;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+    
+    
+}
 
 // @todo: Вывести карточки на страницу
