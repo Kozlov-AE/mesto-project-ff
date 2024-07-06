bca66130ff2828f7569000aa34d8122023703d93
Kozlov-AE <allek-007@list.ru>
2024-07-06 09:40:41 +0300

    Исправления Итерация-1

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,40 +1,33 @@
-import {createCard} from "./index.js";
-import {deleteCard} from "./index.js";
-import {updateCardList} from "./index.js";
 
-const initialCards = [
+export const initialCards = [
     {
         name: "Архыз",
         link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+        alt: "Горный пейзаж Архыза"
     },
     {
         name: "Челябинская область",
         link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+        alt: "Живописный пейзаж Челябинской области с озером и лесистыми берегами"
     },
     {
         name: "Иваново",
         link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+        alt: "Городской пейзаж Иваново"
     },
     {
         name: "Камчатка",
         link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+        alt: "Живописный пейзаж Камчатки с вулканом"
     },
     {
         name: "Холмогорский район",
         link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+        alt: "Панорамный вид на реку и леса Холмогорского района"
     },
     {
         name: "Байкал",
         link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+        alt: "Живописный вид на озеро Байкал"
     }
 ]
-
-function fillCards() {
-    const cards = [];
-    initialCards.forEach(card => {
-        cards.push(createCard(card.name, card.link, deleteCard));
-    });
-    updateCardList(cards); 
-}
-
-fillCards();
--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,42 @@
-const cardtemplate = document.querySelector('#card-template').content;
+import {initialCards} from "./cards.js";
+
+const cardTemplate = document.querySelector('#card-template').content;
 
 const placesList = document.querySelector('.places__list');
 
-export function createCard(name, link, deleteCardFunction) {
-    const templClone = cardtemplate.querySelector('.card').cloneNode(true);
+export function createCard(card, deleteCardFunction) {
+    const templClone = cardTemplate.querySelector('.card').cloneNode(true);
     const delButton = templClone.querySelector('.card__delete-button');
 
     delButton.addEventListener('click', evt => deleteCardFunction(evt));
 
     const tTitle = templClone.querySelector('.card__title');
-    tTitle.insertAdjacentText = name;
+    tTitle.textContent = card.name;
 
     const tImg = templClone.querySelector('.card__image');
-    tImg.src = link;
+    tImg.src = card.link;
+    tImg.alt = card.alt;
     
     return templClone;
 }
 
 export function deleteCard(evt) {
     const card = evt.target.parentElement;
-    const list = document.querySelector('.places__list');
-    list.removeChild(card);
+    placesList.removeChild(card);
+}
+
+function loadCards(){
+    const cards = [];
+    initialCards.forEach(card => {
+        cards.push(createCard(card, deleteCard));
+    })   
+    updateCardList(cards);
 }
 
 export function updateCardList(cards) {
-    const list = document.querySelector('.places__list');
     for (const card of cards) {
-        list.append(card);
+        placesList.append(card);
     }
 }
+
+loadCards();
