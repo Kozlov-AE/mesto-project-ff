2b46f412b4a1ebf74378afc2aabbff4d411943cd
Kozlov-AE <allek-007@list.ru>
2024-07-24 06:32:36 +0300

    Начал работу с попапами

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,24 @@ import {initialCards} from "./cards.js";
 import '../pages/index.css';
 
 const cardTemplate = document.querySelector('#card-template').content;
-
 const placesList = document.querySelector('.places__list');
 
-export function createCard(card, deleteCardFunction) {
+const editButton = document.querySelector('.profile__edit-button');
+const plusButton = document.querySelector('.profile__add-button');
+
+const editPopup = document.querySelector('.popup_type_edit');
+const newCardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
+
+editButton.addEventListener('click', (e) => {
+    openModal(editPopup)
+    const closeButton = editPopup.querySelector('.popup__close');
+    closeButton.addEventListener('click', closeModal);
+    // closeButton.removeEventListener('click', closeModal);
+});
+plusButton.addEventListener('click', (e) => openModal(newCardPopup))
+
+function createCard(card, deleteCardFunction) {
     const templClone = cardTemplate.querySelector('.card').cloneNode(true);
     const delButton = templClone.querySelector('.card__delete-button');
 
@@ -17,27 +31,35 @@ export function createCard(card, deleteCardFunction) {
     const tImg = templClone.querySelector('.card__image');
     tImg.src = card.link;
     tImg.alt = card.alt;
-    
+
     return templClone;
 }
 
-export function deleteCard(evt) {
+function deleteCard(evt) {
     const card = evt.target.parentElement;
     placesList.removeChild(card);
 }
 
-function loadCards(){
+function loadCards() {
     const cards = [];
     initialCards.forEach(card => {
         cards.push(createCard(card, deleteCard));
-    })   
+    })
     updateCardList(cards);
 }
 
-export function updateCardList(cards) {
+function updateCardList(cards) {
     for (const card of cards) {
         placesList.append(card);
     }
 }
 
 loadCards();
+
+function openModal(window) {
+    window.classList.add('popup_is-opened');
+}
+
+function closeModal(window) {
+    window.classList.remove('popup_is-opened');
+}
