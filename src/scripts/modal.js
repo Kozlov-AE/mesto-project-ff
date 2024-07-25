14d21959868107462879d7c5927cb2d3fc56a4e1
Kozlov-AE <allek-007@list.ru>
2024-07-25 06:11:10 +0300

    Закрытие модального окна на ESC

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,9 +2,16 @@ export function openModal(window) {
     window.classList.add('popup_is-opened');
     const closeButton = window.querySelector('.popup__close');
     closeButton.addEventListener('click', x=> closeModal(window));
-    closeButton.removeEventListener('click', x => closeModal(window));
+    window.addEventListener('click', e => overlayClick(e, window));
+    document.addEventListener('keydown', e => closeModal(window));
 }
 
 function closeModal(window) {
     window.classList.remove('popup_is-opened');
+}
+
+function overlayClick(e, window){
+    if (e.target.classList.contains('popup')) {
+        closeModal(window)
+    }
 }
