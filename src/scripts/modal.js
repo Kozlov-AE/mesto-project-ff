17f3787a7516440308ec80d956f6dcf16830119a
Kozlov-AE <allek-007@list.ru>
2024-07-27 21:30:09 +0300

    Fixed close modal by esc

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,7 +1,16 @@
-export function openModal(window) {
+function closeByEsc(event) {
+    if (event.key === 'Escape') {
+        const window = document.querySelector('popup_is-opened');
+        closeModal(window);
+    }
+}
+
+export function openModal(window){
     window.classList.add('popup_is-opened');
+    document.addEventListener('keydown', closeByEsc);
 }
 
 export function closeModal(window) {
+    document.removeEventListener('keydown', closeByEsc);
     window.classList.remove('popup_is-opened');
 }
